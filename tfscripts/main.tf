abd9535a6970770f053d63a46de6391485533761
SeemaYadav84 <88224672+SeemaYadav84@users.noreply.github.com>
2024-05-18 20:06:03 +0530

    Update main.tf

--- tfscripts/main.tf
+++ tfscripts/main.tf
@@ -2,11 +2,11 @@ resource "aws_instance" "EC2-server"{
   ami = "ami-09040d770ffe2224f"
   instance_type = "t2.micro"
   key_name = "TF_key"
-  vpc_security_group_ids= ["sg-084e9b1aea742d17c"]
+  vpc_security_group_ids= ["sg-040e4d07776a9f29f"]
   connection {
     type     = "ssh"
     user     = "ubuntu"
-    private_key = file("./Ansible_host_key.pem")
+    private_key = "TF_key"
     host     = self.public_ip
   }
   provisioner "remote-exec" {
@@ -19,7 +19,7 @@ resource "aws_instance" "EC2-server"{
         command = " echo ${aws_instance.EC2-server.public_ip} > inventory "
   }
    provisioner "local-exec" {
-  command = "ansible-playbook /var/lib/jenkins/workspace/Banking-Pipeline/scripts/Banking-playbook.yml "
+  command = "ansible-playbook /var/lib/jenkins/workspace/Banking-Pipeline/tfscripts/Banking-playbook.yml "
   } 
 }
 
