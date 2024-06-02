4e6ce5b24564b1c0238268ea3eebaaa4816f8607
SeemaYadav84 <88224672+SeemaYadav84@users.noreply.github.com>
2024-06-02 19:59:14 +0530

    Update main.tf

--- tfscripts/main.tf
+++ tfscripts/main.tf
@@ -1,28 +1,23 @@
-locals {
-  ssh_user         = "ubuntu"
-  key_name         = "devops"
-  private_key_path = "/home/ubuntu/.ssh/id_rsa"
-}
-
 resource "aws_instance" "EC2-server" {
   ami = "ami-09040d770ffe2224f"
   instance_type = "t2.micro"
   key_name = "web1-key"
   vpc_security_group_ids= ["sg-040e4d07776a9f29f"]
+  
+  tags = {
+    Name = "EC2-server"
+  }
+  
+  provisioner "remote-exec" {
+    inline = [ "echo 'wait to start instance' "]
+  }
+
   connection {
     type     = "ssh"
     user     = "ubuntu"
     private_key = tls_private_key.web1-key.private_key_pem
     host     = self.public_ip
     }
-  
-  provisioner "remote-exec" {
-    inline = [ "echo 'wait to start instance' "]
-  }
-
-  tags = {
-    Name = "EC2-server"
-  }
 
   provisioner "local-exec" {
         command = " echo ${aws_instance.EC2-server.public_ip} > inventory "
