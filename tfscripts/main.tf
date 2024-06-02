cfc6c10c5293cf9e1d826aec0ba34863cc3558ae
SeemaYadav84 <88224672+SeemaYadav84@users.noreply.github.com>
2024-06-03 00:20:14 +0530

    Update main.tf

--- tfscripts/main.tf
+++ tfscripts/main.tf
@@ -36,7 +36,7 @@ resource "aws_key_pair" "app-key" {
   public_key = tls_private_key.web1-key.public_key_openssh
 }
 #Private key 
-resource "local_file" "web1-key" {
+resource "local_sensitive_file" "web1-key" {
   content  = tls_private_key.web1-key.private_key_pem
   file_permission = "600"
   directory_permission = "700"
