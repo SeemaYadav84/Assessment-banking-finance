5a817a5dd5785da9df1916339941b30e7f64094e
SeemaYadav84 <88224672+SeemaYadav84@users.noreply.github.com>
2024-06-06 18:43:33 +0530

    Update main.tf

--- tfscripts/main.tf
+++ tfscripts/main.tf
@@ -30,7 +30,7 @@ connection {
 "sudo apt update",
 "curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64",
 "sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64",
-"sudo apt install docker.io",
+"sudo apt install docker.io -y",
 "curl -LO https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl",
 "curl -LO https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256",
 "echo $(cat kubectl.sha256)  kubectl | sha256sum –check",
