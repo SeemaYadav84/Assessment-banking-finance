1a73bc9fcb1cec6b41fa72e6a661658671bb1990
SeemaYadav84 <88224672+SeemaYadav84@users.noreply.github.com>
2024-06-06 18:50:13 +0530

    Update main.tf

--- tfscripts/main.tf
+++ tfscripts/main.tf
@@ -33,7 +33,6 @@ connection {
 "sudo apt install docker.io -y",
 "curl -LO https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl",
 "curl -LO https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256",
-"echo $(cat kubectl.sha256)  kubectl | sha256sum –check",
 "sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl",
 "sudo usermod -aG docker ubuntu",
 "newgrp docker",
