8e79ccfad92be2cfddac1ad8bbc96d8f22dfbd43
SeemaYadav84 <88224672+SeemaYadav84@users.noreply.github.com>
2024-06-08 21:59:08 +0530

    Update main.tf

--- tfscripts/main.tf
+++ tfscripts/main.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "EC2-server" {
-  ami = "ami-09040d770ffe2224f"
+  ami = "ami-04b70fa74e45c3917"
   instance_type = "t2.medium"
   key_name = "web1-key"
-  vpc_security_group_ids= ["sg-040e4d07776a9f29f"]
+  vpc_security_group_ids= ["sg-04abe48ace52cdbac"]
   
   tags = {
     Name = "EC2-server"
